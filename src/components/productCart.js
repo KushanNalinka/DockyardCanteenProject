94acea89736e504d84b0d9c11927b7451ef04713
Lahiru Rajakaruna Jayasinghe <140371042+LahiruJ-2003@users.noreply.github.com>
2024-07-26 11:45:38 +0530

    Update productCart.js
    changed css
    . reduce the sizes
    . changed button
    . now only 8 items shown on display

--- src/components/productCart.js
+++ src/components/productCart.js
@@ -176,6 +176,187 @@ export default ProductCart*/
 // };
 
 // export default ProductCart;
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import iconCart from '../assets/images/iconCart.png';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { addToCart } from '../stores/cart';
+
+// const ProductCart = (props) => {
+//     const carts = useSelector(store => store.cart.items);
+//     const { id, name, price, image, category, meal, slug } = props.data;
+//     const mealText = Array.isArray(meal) ? meal.join(', ') : meal;
+
+//     const dispatch = useDispatch();
+//     const handleAddToCart = () => {
+//         dispatch(addToCart({
+//             productId: id,
+//             quantity: 1
+//         }));
+//         // Open the cart tab
+//     };
+
+//     return (
+//         <div className='bg-white p-2 rounded-xl shadow-lg w-36 h-48 transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between'>
+//             <Link to={slug}>
+//                 <img src={image} alt={name} className='w-full h-24 object-cover rounded-t-xl' />
+//             </Link>
+//             <div className='p-1 flex flex-col flex-grow'>
+//                 <h3 className='text-sm font-bold'>{name}</h3>
+//                 <div className='flex justify-between items-center mt-1'>
+//                     <p className='text-sm font-medium'>
+//                         LKR <span>{price}</span>
+//                     </p>
+//                     <button 
+//                         className='bg-[#FFC10C] p-1 rounded-full text-xs flex items-center gap-1 hover:bg-[#35a436]' 
+//                         onClick={handleAddToCart}
+//                     >
+//                         <span className='font-semibold'>Add</span>
+//                         <img src={iconCart} alt="cart icon" className='w-3' />
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ProductCart;
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import iconCart from '../assets/images/iconCart.png';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { addToCart } from '../stores/cart';
+
+// const ProductCart = (props) => {
+//     const carts = useSelector(store => store.cart.items);
+//     const { id, name, price, image, category, meal, slug } = props.data;
+//     const mealText = Array.isArray(meal) ? meal.join(', ') : meal;
+
+//     const dispatch = useDispatch();
+//     const handleAddToCart = () => {
+//         dispatch(addToCart({
+//             productId: id,
+//             quantity: 1
+//         }));
+//         // Open the cart tab
+//     };
+
+//     return (
+//         <div className='bg-white p-3 rounded-lg shadow-md w-40 h-56 transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between'>
+//             <Link to={slug}>
+//                 <img src={image} alt={name} className='w-full h-24 object-cover rounded-t-lg' />
+//             </Link>
+//             <div className='p-1 flex flex-col justify-between flex-grow'>
+//                 <h3 className='text-xs font-bold text-left'>{name}</h3>
+//                 <div className='flex justify-between items-center mt-1'>
+//                     <p className='text-xs font-medium text-left'>
+//                         LKR <span>{price}</span>
+//                     </p>
+//                     <button 
+//                         className='bg-[#FFC10C] p-1 rounded-full text-xs flex items-center gap-1 hover:bg-[#35a436]' 
+//                         onClick={handleAddToCart}
+//                     >
+//                         <span className='font-semibold'>Add</span>
+//                         <img src={iconCart} alt="cart icon" className='w-3' />
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ProductCart;
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import iconCart from '../assets/images/iconCart.png';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { addToCart } from '../stores/cart';
+
+// const ProductCart = (props) => {
+//     const carts = useSelector(store => store.cart.items);
+//     const { id, name, price, image, category, meal, slug } = props.data;
+//     const mealText = Array.isArray(meal) ? meal.join(', ') : meal;
+
+//     const dispatch = useDispatch();
+//     const handleAddToCart = () => {
+//         dispatch(addToCart({
+//             productId: id,
+//             quantity: 1
+//         }));
+//         // Open the cart tab
+//     };
+
+//     return (
+//         <div className='bg-white p-4 rounded-lg shadow-md w-48 h-56 transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between'>
+//             <Link to={slug}>
+//                 <img src={image} alt={name} className='w-full h-32 object-cover rounded-t-lg' />
+//             </Link>
+//             <div className='p-2 flex flex-col justify-between flex-grow'>
+//                 <h3 className='text-sm font-bold text-left'>{name}</h3>
+//                 <div className='flex justify-between items-center mt-2'>
+//                     <p className='text-sm font-medium text-left'>
+//                         LKR <span>{price}</span>
+//                     </p>
+//                     <button 
+//                         className='bg-[#FFC10C] p-2 rounded-full text-xs flex items-center gap-2 hover:bg-[#35a436]' 
+//                         onClick={handleAddToCart}
+//                     >
+//                         <span className='font-semibold'>Add</span>
+//                         <img src={iconCart} alt="cart icon" className='w-4' />
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ProductCart;
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import iconCart from '../assets/images/iconCart.png';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { addToCart } from '../stores/cart';
+
+// const ProductCart = (props) => {
+//     const carts = useSelector(store => store.cart.items);
+//     const { id, name, price, image, category, meal, slug } = props.data;
+//     const mealText = Array.isArray(meal) ? meal.join(', ') : meal;
+
+//     const dispatch = useDispatch();
+//     const handleAddToCart = () => {
+//         dispatch(addToCart({
+//             productId: id,
+//             quantity: 1
+//         }));
+//         // Open the cart tab
+//     };
+
+//     return (
+//         <div className='bg-white p-4 rounded-lg shadow-md w-48 h-56 transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between'>
+//             <Link to={slug}>
+//                 <img src={image} alt={name} className='w-full h-24 object-cover rounded-t-lg' />
+//             </Link>
+//             <div className='p-2 flex flex-col justify-between flex-grow'>
+//                 <h3 className='text-sm font-bold text-left'>{name}</h3>
+//                 <div className='flex justify-between items-center mt-2'>
+//                     <p className='text-sm font-medium text-left'>
+//                         LKR <span>{price}</span>
+//                     </p>
+//                     <button 
+//                         className='bg-[#FFC10C] p-2 rounded-full text-xs flex items-center gap-2 hover:bg-[#35a436]' 
+//                         onClick={handleAddToCart}
+//                     >
+//                         <span className='font-semibold'>Add</span>
+//                         <img src={iconCart} alt="cart icon" className='w-4' />
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ProductCart;
 import React from 'react';
 import { Link } from 'react-router-dom';
 import iconCart from '../assets/images/iconCart.png';
@@ -197,9 +378,9 @@ const ProductCart = (props) => {
     };
 
     return (
-        <div className='bg-white p-4 rounded-xl shadow-xl w-48 h-64 transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between'>
+        <div className='bg-white p-4 rounded-lg shadow-md w-48 h-56 transform transition-transform duration-300 hover:scale-105 flex flex-col justify-between'>
             <Link to={slug}>
-                <img src={image} alt={name} className='w-full h-32 object-cover rounded-t-xl' />
+                <img src={image} alt={name} className='w-full h-24 object-cover rounded-t-lg' />
             </Link>
             <div className='p-2 flex flex-col justify-between flex-grow'>
                 <h3 className='text-sm font-bold text-left'>{name}</h3>
@@ -208,11 +389,11 @@ const ProductCart = (props) => {
                         LKR <span>{price}</span>
                     </p>
                     <button 
-                        className='bg-[#FFC10C] p-1 rounded-full text-xs flex items-center gap-2 hover:bg-[#35a436]' 
+                        className='bg-[#ff0f0f] p-2 rounded-full text-xs flex items-center gap-2 hover:bg-[#35a436] shadow-lg' 
                         onClick={handleAddToCart}
                     >
-                        <span>Add</span>
-                        <img src={iconCart} alt="cart icon" className='w-3' />
+                        <span className='font-bold text-white w-12'>Add</span>
+                        {/* <img src={iconCart} alt="cart icon" className='w-4' /> */}
                     </button>
                 </div>
             </div>

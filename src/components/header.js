560d5067dfa9f0fb11995f89c232281fc2a74d95
Lahiru Rajakaruna Jayasinghe <140371042+LahiruJ-2003@users.noreply.github.com>
2024-07-29 08:23:56 +0530

    Update header.js
    css update changes in the nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -78,16 +78,14 @@ const Header = () => {
 
 export default Header;*/
 import React, { useState, useEffect } from 'react';
-import { Link ,useParams} from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import iconCart from '../assets/images/iconCart.png';
 import logo from '../assets/images/doccanteen.png.png';
-import { useSelector, useDispatch } from 'react-redux';
-
+import { useSelector } from 'react-redux';
 
 const Header = () => {
     const { meal } = useParams();
 
-    
     const [totalQuantity, setTotalQuantity] = useState(0);
     const carts = useSelector(store => store.cart.items);
 
@@ -97,12 +95,13 @@ const Header = () => {
         setTotalQuantity(total);
     }, [carts]);
 
-    
     return (
-        <header className='flex justify-between items-center mb-5 bg-[#FFC10C] p-4 '>
+        <header className='flex justify-between items-center mb-5 bg-[#FFC10C] p-4'>
             <div className='flex items-center'>
-                <img src={logo} alt="Logo" className='w-10 h-10 mr-2' />
-                <h3  className='text-lg font-semibold  text-white'>DOC CAFE CANTEEN</h3>
+                <img src={logo} alt="Logo" className='w-12 h-12 mr-2' />
+                <h3 className='text-lg font-semibold text-white'>
+                    <span className='text-2xl font-bold'>DOCCAFÉ</span> <span className='text-lg'></span>
+                </h3>
             </div>
             {/* <h6 className="text-4xl font-bold mb-1 text-white">{meal.charAt(0).toUpperCase() + meal.slice(1)}</h6> */}
             <div className='w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative'>
@@ -112,8 +111,7 @@ const Header = () => {
                 </span>
             </div>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
-
